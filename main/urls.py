22445279aa59a8ac75b2db461ebd11513ca1fc98
rj33536 <rj33536@gmail.com>
2020-03-24 15:37:12 +0530

    added post functionality. templates are still shitty

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('login',views.login,name="login"),
     path('logout',views.logout,name="logout"),
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('post',views.post_blog,name="post"),
+    path('view/<int:id>',views.view_details,name="detail")
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,19 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate,login as auth_login,logout as auth_logout
 from .models import blog
 from django.urls import reverse
+from django.utils import timezone
 # Create your views here.
+
+def view_details(request,id):
+	if not request.user.is_authenticated:
+		return render(request,'login.html',{"message":"You need to login first before posting"})
+	context = {
+		"blog":blog.objects.get(id=id),
+		"user":request.user
+		}
+	print(context)
+	return render(request,"view.html",context=context)
+
 def login(request):
 	username = request.POST["username"]
 	password = request.POST["password"]
@@ -17,13 +29,32 @@ def logout(request):
 	auth_logout(request)
 	return render(request,"login.html",{"message":"Successfully logged out"})
 
+def post_blog(request):
+	if not request.user.is_authenticated:
+		return render(request,'login.html',{"message":"You need to login first before posting"})
+
+	try:
+		text = request.POST["content"]
+		title=request.POST["title"]
+		myblog = blog(author=request.user,title=title,text=text,created_date=timezone.now())
+		myblog.publish()
+		print(myblog)
+		return HttpResponseRedirect(reverse("index"))
+	except Exception as e:
+		print(e)
+		return render(request,'post.html')
+	
+
 
 def index(request):
 	if not request.user.is_authenticated:
     		return render(request,'login.html',{"message":None})
+	user = request.user
+	MyBlogs = blog.objects.filter(author = user)
 	context={
 		'blogs':blog.objects.all(),
-		'user':request.user,
+		'MyBlogs':MyBlogs,  
+		'user':user,
 	}
 	return render(request,'index.html',context=context)
 	
