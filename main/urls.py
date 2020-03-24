cf6730f094e5b712c5418e3a4168fc0d0d86eb1b
rj33536 <rj33536@gmail.com>
2020-03-24 14:04:49 +0530

    added basic authentication

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.login)
+    path('login',views.login,name="login"),
+    path('logout',views.logout,name="logout"),
+    path('',views.index,name="index")
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate,login as auth_login,logout as auth_logout
 from .models import blog
+from django.urls import reverse
 # Create your views here.
 def login(request):
+	username = request.POST["username"]
+	password = request.POST["password"]
+	user = authenticate(request,username=username,password = password)
+	if user is not None:
+		auth_login(request,user)
+		return HttpResponseRedirect(reverse("index"))
+	return render(request,"login.html",{"message":"Invalid Credentials"})
+
+def logout(request):
+	auth_logout(request)
+	return render(request,"login.html",{"message":"Successfully logged out"})
+
+
+def index(request):
+	if not request.user.is_authenticated:
+    		return render(request,'login.html',{"message":None})
 	context={
-		'blogs':blog.objects.all()
+		'blogs':blog.objects.all(),
+		'user':request.user,
 	}
 	return render(request,'index.html',context=context)
+	
