e9f66e38b1160d02227cf702b501ae9dfd06befb
rj33536 <rj33536@gmail.com>
2020-05-08 13:49:52 +0530

    heroku

--- medium/settings.py
+++ medium/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'medium/media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
