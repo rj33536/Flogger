04931195a84e0ffeedf25ba51e4c7934a35d8359
rj33536 <rj33536@gmail.com>
2020-05-12 09:43:25 +0530

    web master

--- medium/settings.py
+++ medium/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sb)jfna0v^g8_p^m!!!@#mm4qv^vtgqiz!fj)o)!39!fsy-x1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1','flogger101.herokuapp.com'
